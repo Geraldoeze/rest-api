2a0354ff777a6a6585ac1b4a2544eb0e68256bbe
Geraldoeze <egerald344@gmail.com>
2022-03-13 18:38:07 +0100

    server

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //application/json
+
+app.use((req, res, next) => {
+    //CORS error handler
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next();
+})
 
 app.use('/feed', feedRoutes)
 
--- routes/feed.js
+++ routes/feed.js
@@ -5,6 +5,9 @@ const feedController = require('../controllers/feed')
 const router = express.Router();
 
 //GET /feed/posts
-router.get('/posts', feedController.getPosts)
+router.get('/posts', feedController.getPosts);
+
+//POST /feed/posts
+router.post('/post', feedController.createPost);
 
 module.exports = router;
