94f7870dda687dea19f3dde2e4e2f0621c473c12
Geraldoeze <egerald344@gmail.com>
2022-03-17 00:46:49 +0100

    working on db

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
@@ -11,6 +12,7 @@ const MONGODB_URI =
 
 
 app.use(bodyParser.json()); //application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     //CORS error handler
@@ -20,10 +22,18 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/feed', feedRoutes)
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
+         console.log('CONNECTED')
         app.listen(4500); 
     })
     .catch(err => console.log(err))
