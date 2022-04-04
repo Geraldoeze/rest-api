5c86b8fc0de40e8a8f2daa141890f0a4d400a2e1
Gerald Ezenagu <egerald344@gmail.com>
2022-04-04 21:41:39 +0100

    websocket error

--- controllers/feed.js
+++ controllers/feed.js
@@ -13,6 +13,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments()
     const posts = await Post.find()
         .populate('creator')
+        .sort({createdAt: -1})
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
 
@@ -88,7 +89,7 @@ exports.getPost = (req, res, next) => {
         })
 }
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const postId = req.params.postId;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -107,42 +108,40 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
-    Post.findById(postId)
-        .then(post => {
+    try { 
+        const post = await Post.findById(postId).populate('creator');
             if (!post) {
             const error = new Error('Could not find post.')
             error.statusCode = 404;
             throw error;
             }
-            if(post.creator.toString() !== req.userId){
+            if(post.creator._id.toString() !== req.userId){
                 const error = new Error('Not authorized');
                 error.statusCode = 403;
                 throw error;
             }
             if(imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
-            }
+            }  
             post.title = title;
             post.imageUrl = imageUrl;
             post.content = content;
-            return post.save();
-        })
-        .then(result => {
+            const result = await post.save();
+            io.getIO().emit('posts', {action: "update", post: result})
             res.status(200).json({message: 'Post Updated', post: result});
-        })
-        .catch (err => {
-    if (!err.statusCode) {
-        err.statusCode = 500;
+        } catch (err) {
+            if (!err.statusCode) {
+             err.statusCode = 500;
     }
     next(err);
-})
+}
 };
 
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then(post => {
+  try{  
+    const post = await Post.findById(postId)
             if (!post) {
                 const error = new Error('Could not find post.')
                 error.statusCode = 404;
@@ -155,23 +154,18 @@ exports.deletePost = (req, res, next) => {
             }
             //Future code
             clearImage(post.imageUrl);
-            return Post.findByIdAndRemove(postId)
-        })
-        .then(result => {
-            return User.findById(req.userId)
-        })
-        .then(user => {
+            await Post.findByIdAndRemove(postId);
+
+        const user = await User.findById(req.userId);
             user.posts.pull(postId);
-            return user.save();
-        })
-        .then( result => {
+            await user.save();
+            io.getIO().emit('posts', { action: 'delete', post: postId })
             res.status(200).json({ message: 'Deleted post'});
-        })
-        .catch(err => {
+        } catch(err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
             }
-        });
+    };
 
 }
 
