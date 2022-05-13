982ab761e05c9ab6d9046c5aec9b324ae893b15c
Gerald Ezenagu <egerald344@gmail.com>
2022-05-13 23:23:42 +0100

    make it in life

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// // const multer = require('multer');
+const multer = require('multer');
 
 
 const feedRoutes = require('./routes/feed');
@@ -10,25 +10,25 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-// // const fileStorage = multer.diskStorage({
-// //     destination: (req, file, cb) => {
-// //         cb(null, 'images');
-// //     },
-// //     filename: (req, file, cb) => {
-// //         cb(null,  file.originalname);
-// //     } 
-// // })
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null,  file.originalname);
+    } 
+})
 
-// // const fileFilter = (req, file, cb) => {
-// //     if (file.mimetype === 'image/png' ||
-// //         file.mimetype === 'image/jpg' ||
-// //         file.mimetype === 'image/jpeg'
-// //     ) {
-// //         cb(null, true);
-// //     } else {
-// //         cb(null, false);
-// //     }
-// // }
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
 
 
 const MONGODB_URI =
@@ -38,22 +38,22 @@ const URI = 'mongodb://127.0.0.1:27017/node-clus'
 
 app.use(bodyParser.json()); //application/json
 
-// // app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
-// // app.use(express.static(path.join(__dirname, 'public')));
-// // app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// // app.use((req, res, next) => {
-// //     //CORS error handler
-// //     res.setHeader('Access-Control-Allow-Origin', '*', 'OPTIONS');
-// //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTIONS');
-// //     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-// //     next();
-// // })
+app.use((req, res, next) => {
+    //CORS error handler
+    res.setHeader('Access-Control-Allow-Origin', '*', 'OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next();
+})
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server is Running!')
   })
 
 // // app.use((error, req, res, next) => {
