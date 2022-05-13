6aeabf40054674a0046932163f69790574b6b500
Gerald Ezenagu <egerald344@gmail.com>
2022-05-13 23:21:34 +0100

    make it

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
+// // const multer = require('multer');
 
 
 const feedRoutes = require('./routes/feed');
@@ -10,25 +10,25 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null,  file.originalname);
-    } 
-})
+// // const fileStorage = multer.diskStorage({
+// //     destination: (req, file, cb) => {
+// //         cb(null, 'images');
+// //     },
+// //     filename: (req, file, cb) => {
+// //         cb(null,  file.originalname);
+// //     } 
+// // })
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+// // const fileFilter = (req, file, cb) => {
+// //     if (file.mimetype === 'image/png' ||
+// //         file.mimetype === 'image/jpg' ||
+// //         file.mimetype === 'image/jpeg'
+// //     ) {
+// //         cb(null, true);
+// //     } else {
+// //         cb(null, false);
+// //     }
+// // }
 
 
 const MONGODB_URI =
@@ -38,47 +38,50 @@ const URI = 'mongodb://127.0.0.1:27017/node-clus'
 
 app.use(bodyParser.json()); //application/json
 
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+// // app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+// // app.use(express.static(path.join(__dirname, 'public')));
+// // app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-    //CORS error handler
-    res.setHeader('Access-Control-Allow-Origin', '*', 'OPTIONS');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next();
-})
+// // app.use((req, res, next) => {
+// //     //CORS error handler
+// //     res.setHeader('Access-Control-Allow-Origin', '*', 'OPTIONS');
+// //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTIONS');
+// //     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+// //     next();
+// // })
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 app.get('/', (req, res) => {
-    res.send('Server is Running!')
+    res.send('Hello World!')
   })
 
-app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({message: message, data: data });
-})
-const port = process.env.PORT || 3000
+// // app.use((error, req, res, next) => {
+// //     console.log(error);
+// //     const status = error.statusCode || 500;
+// //     const message = error.message;
+// //     const data = error.data;
+// //     res.status(status).json({message: message, data: data });
+// // })
+const port = process.env.PORT || 4500
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true
 })
 .then(result => {
   console.log('Connected')
-    const server = app.listen(4500);
-   const io = require("./socket").init(server); 
-  io.on('connection', socket => {
-      console.log('Client connected', socket.id);
-  })
- })
-.catch(err => {
-  console.log(err);
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })})
+//   const server = app.listen(4500);
+//   const io = require("./socket").init(server); 
+//   io.on('connection', socket => {
+//       console.log('Client connected', socket.id);
+//   })
+// })
+// .catch(err => {
+//   console.log(err);
+// })
 
 
 
