3f7674edf9856b381ff3439c448725050f9644b9
Gerald Ezenagu <egerald344@gmail.com>
2022-11-29 00:40:05 +0100

    added engines

--- models/post.js
+++ models/post.js
@@ -14,7 +14,7 @@ const postSchema = new Schema({
         type: String,
         require: true
     },
-    creator: {
+    creator: { 
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
